0775bf5bb42cfb0517c9a84cea3247f7ea1d4fdd
Congzhou <526691901@qq.com>
2022-01-23 13:20:00 +0800

    更新暂停和结束面板

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -242,7 +242,8 @@ public class Player : MonoBehaviour
     }
     // helper functions
     public void GameOver() 
-    { 
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
         // todo: change scene
     }
     // energy loss 
